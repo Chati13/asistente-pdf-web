cc91499d0c06a8882c9dd455c5d5121958de69d5
Chati13 <informaticos@drasanvi.com>
2025-08-12 15:03:22 +0200

    Update next.config.js

--- asistente-pdf-web/next.config.js
+++ asistente-pdf-web/next.config.js
@@ -1,3 +1,10 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    // Fuerza alias '@' a la raíz del proyecto que Vercel está construyendo
+    config.resolve.alias["@"]= path.resolve(__dirname);
+    return config;
+  },
+};
